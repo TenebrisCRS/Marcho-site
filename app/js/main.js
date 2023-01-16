996bc931008c993d3fd53946fd278f88ebbd63cd
TenebrisCRS <uruso.leo@gmail.com>
2023-01-16 20:31:09 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,14 @@ $(function () {
     arrows: false,
     fade:true,
     autoplay: true,
-  })
-})
+  });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ function scripts() {                              //Функция для скр
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))                    //Перемещаем в файл при помощи плакина gulp-concat
